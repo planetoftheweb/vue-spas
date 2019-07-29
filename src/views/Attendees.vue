994ac0af3f6cee064ffc36095ba84dbebebf66d6
Ray Villalobos <ray@planetoftheweb.com>
2019-07-29 16:09:07 -0400

    finished meeting log

--- src/views/Attendees.vue
+++ src/views/Attendees.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mt-4">
-    <div class="row justify-content-center" v-if="user">
+    <div class="row justify-content-center" v-if="user && user.uid == userID">
       <div class="col-md-8">
         <h1 class="font-weight-light text-center">Attendees</h1>
 
@@ -9,15 +9,24 @@
             <div class="input-group input-group-lg">
               <input
                 type="text"
-                name="searchQuery"
                 placeholder="Search Attendees"
                 class="form-control"
+                v-model="searchQuery"
+                ref="searchQuery"
               />
               <div class="input-group-append">
-                <button class="btn btn-sm btn-outline-info" title="Pick a random attendee">
+                <button
+                  class="btn btn-sm btn-outline-info"
+                  title="Pick a random attendee"
+                  @click="chooseRandom"
+                >
                   <font-awesome-icon icon="random"></font-awesome-icon>
                 </button>
-                <button class="btn btn-sm btn-outline-info" title="Reset Search">
+                <button
+                  class="btn btn-sm btn-outline-info"
+                  title="Reset Search"
+                  @click="resetQuery"
+                >
                   <font-awesome-icon icon="undo"></font-awesome-icon>
                 </button>
               </div>
@@ -30,13 +39,18 @@
     <div class="row justify-content-center">
       <div
         class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
-        v-for="item in attendees"
+        v-for="item in filteredAttendees"
         :key="item.id"
       >
         <div class="card">
           <div class="card-body px-3 py-2 d-flex align-items-center justify-content-center">
-            <div class="btn-group pr-2" v-if="user">
-              <button class="btn btn-sm btn-outline-secondary" title="Give user a star">
+            <div class="btn-group pr-2" v-if="user && (user.uid == userID)">
+              <button
+                class="btn btn-sm"
+                :class="[item.star ? 'text-danger' : '', 'btn-outline-secondary']"
+                title="Give user a star"
+                @click="toggleStar(item.id)"
+              >
                 <font-awesome-icon icon="star"></font-awesome-icon>
               </button>
               <a
@@ -46,7 +60,11 @@
               >
                 <font-awesome-icon icon="envelope"></font-awesome-icon>
               </a>
-              <button class="btn btn-sm btn-outline-secondary" tite="Delete Attendee">
+              <button
+                class="btn btn-sm btn-outline-secondary"
+                tite="Delete Attendee"
+                @click="deleteAttendee(item.id)"
+              >
                 <font-awesome-icon icon="trash"></font-awesome-icon>
               </button>
             </div>
@@ -69,6 +87,8 @@ export default {
     return {
       user: null,
       attendees: [],
+      displayAttendees: [],
+      searchQuery: "",
       userID: this.$route.params.userID,
       meetingID: this.$route.params.meetingID
     };
@@ -94,14 +114,71 @@ export default {
           snapData.push({
             id: doc.id,
             eMail: doc.data().eMail,
-            displayName: doc.data().displayName
+            displayName: doc.data().displayName,
+            star: doc.data().star
           });
         });
         this.attendees = snapData;
+        this.displayAttendees = this.attendees;
       });
   },
   components: {
     FontAwesomeIcon
+  },
+  computed: {
+    filteredAttendees: function() {
+      const dataFilter = item =>
+        item.displayName.toLowerCase().match(this.searchQuery.toLowerCase()) &&
+        true;
+
+      return this.displayAttendees.filter(dataFilter);
+    }
+  },
+  methods: {
+    deleteAttendee: function(attendeeID) {
+      if (this.user && this.user.uid == this.userID) {
+        db.collection("users")
+          .doc(this.user.uid)
+          .collection("meetings")
+          .doc(this.meetingID)
+          .collection("attendees")
+          .doc(attendeeID)
+          .delete();
+      }
+    },
+    chooseRandom() {
+      const randomAttendee = Math.floor(Math.random() * this.attendees.length);
+      this.resetQuery();
+      this.displayAttendees = [this.attendees[randomAttendee]];
+    },
+
+    resetQuery() {
+      (this.displayAttendees = this.attendees), (this.$refs.searchQuery = "");
+      this.searchQuery = "";
+    },
+    toggleStar: function(attendeeID) {
+      if (this.user && this.user.uid == this.userID) {
+        const ref = db
+          .collection("users")
+          .doc(this.user.uid)
+          .collection("meetings")
+          .doc(this.meetingID)
+          .collection("attendees")
+          .doc(attendeeID);
+        ref.get().then(doc => {
+          const star = doc.data().star;
+          if (star) {
+            ref.update({
+              star: !star
+            });
+          } else {
+            ref.update({
+              star: true
+            });
+          }
+        });
+      }
+    }
   }
 };
 </script>
