a178c2e3e2ab7b88688dc66a0cecde7f41102a57
Ray Villalobos <ray@planetoftheweb.com>
2019-07-30 20:29:33 -0400

    adding icons and styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    <h1>Welcome</h1>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col-10 col-md-10 col-lg-8 col-xl-7">
+        <h4 class="display-4 text-primary mt-3 mb-2">Meeting Log</h4>
+        <p class="lead">
+          This simple app creates meetings, allows people to check in, and
+          picks random users to award giveaways. It's a good example of a
+          Single Page Application which includes connection to a database and
+          routing. It's a practical way to learn
+          <a href="https://vuejs.org/">Vue.js</a>
+          with
+          <a href="https://firebase.google.com">Firebase</a>.
+
+          <font-awesome-icon icon="star"></font-awesome-icon>
+        </p>
+
+        <router-link
+          class="btn btn-outline-primary mr-2"
+          to="/register"
+        >Register</router-link>
+        <router-link
+          class="btn btn-outline-primary mr-2"
+          to="/login"
+        >Log In</router-link>
+        <router-link
+          class="btn btn-primary"
+          to="/meetings"
+        >Meetings</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    FontAwesomeIcon
+  }
 };
 </script>
