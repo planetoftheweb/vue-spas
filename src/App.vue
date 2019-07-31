8fe777c1ef734363a370b4d695dba1c070c7c360
Ray Villalobos <ray@planetoftheweb.com>
2019-07-31 15:27:32 -0400

    added firebase

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
   </div>
 </template>
 <script>
-import Navigation from "./components/Navigation.vue";
+import Navigation from "@/components/Navigation.vue";
+import Firebase from "firebase";
+import db from "./db.js";
 
 export default {
   components: {
@@ -16,5 +18,6 @@ export default {
 
 <style lang="scss">
 $primary: #05b2dd;
+$info: #65be1c;
 @import "node_modules/bootstrap/scss/bootstrap";
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,59 @@
 <template>
-  <div class>
-    <h1>Home</h1>
+  <div class=" mt-3">
+    <div class="text-secondary text-center pl-1">Welcome back <span class="font-weight-bold text-info">{{ user }}</span></div>
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-10 col-md-10 col-lg-8 col-xl-7">
+          <h4 class="display-4 text-primary mb-2">Meeting Log</h4>
+          <p class="lead">
+            This simple app creates meetings, allows people to check in, and
+            picks random users to award giveaways. It's a good example of a
+            Single Page Application which includes connection to a database and
+            routing. It's a practical way to learn
+            <a href="https://vuejs.org/">Vue.js</a>
+            with
+            <a href="https://firebase.google.com">Firebase</a>.
+          </p>
+
+          <router-link
+            class="btn btn-outline-primary mr-2"
+            to="/register"
+          >Register</router-link>
+          <router-link
+            class="btn btn-outline-primary mr-2"
+            to="/login"
+          >Log In</router-link>
+          <router-link
+            class="btn btn-primary"
+            to="/meetings"
+          >Meetings</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Firebase from "firebase";
 import Navigation from "@/components/Navigation";
+import db from "../db.js";
 
 export default {
   name: "home",
+  data: function() {
+    return {
+      user: null
+    };
+  },
+  mounted() {
+    db.collection("users")
+      .doc("Lz61xROBHMcJ8K7q16It")
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name;
+      });
+  },
+
   components: {
     Navigation
   }
