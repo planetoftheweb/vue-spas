23e2813ce99a042b16573aa9accbff603a0584aa
Ray Villalobos <ray@planetoftheweb.com>
2019-08-01 16:34:15 -0700

    adding meetings done

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
       class="container"
       :user="user"
       @logout="logout"
+      @addMeeting="addMeeting"
     />
   </div>
 </template>
@@ -32,12 +33,21 @@ export default {
           this.user = null;
           this.$router.push("login");
         });
+    },
+    addMeeting: function(payload) {
+      db.collection("users")
+        .doc(this.user.uid)
+        .collection("meetings")
+        .add({
+          name: payload,
+          createdAt: Firebase.firestore.FieldValue.serverTimestamp()
+        });
     }
   },
   mounted() {
     Firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.user = user.displayName;
+        this.user = user;
       }
     });
   },
--- src/views/Meetings.vue
+++ src/views/Meetings.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    <h1>Meetings</h1>
+  <div class="container mt-4">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-9 col-lg-7">
+        <h1 class="font-weight-light text-center">Add a Meeting</h1>
+
+        <div class="card bg-light">
+          <div class="card-body text-center">
+            <form class="formgroup">
+              <div class="input-group input-group-lg">
+                <input
+                  type="text"
+                  class="form-control"
+                  name="meetingName"
+                  placeholder="Meeting name"
+                  aria-describedby="buttonAdd"
+                  v-model="meetingName"
+                  ref="meetingName"
+                />
+                <div class="input-group-append">
+                  <button
+                    type="submit"
+                    class="btn btn-sm btn-info"
+                    id="buttonAdd"
+                    @click.prevent="handleAdd"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: "meetings",
+  data: function() {
+    return {
+      meetingName: null
+    };
+  },
+  methods: {
+    handleAdd: function() {
+      this.$emit("addMeeting", this.meetingName);
+      this.meetingName = null;
+      this.$refs.meetingName.focus();
+    }
+  },
+  props: ["user"]
+};
+</script>
