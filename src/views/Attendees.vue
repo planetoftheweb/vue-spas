32dc69a2eaa3d16453dc5c793c5103c697ec434e
Ray Villalobos <ray@planetoftheweb.com>
2019-08-04 15:42:13 -0700

    filtered attendees

--- src/views/Attendees.vue
+++ src/views/Attendees.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="container mt-4">
+
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h1 class="font-weight-light text-center">Attendees</h1>
+
+        <div class="card bg-light mb-4">
+          <div class="card-body text-center">
+            <div class="input-group input-group-lg">
+              <input
+                type="text"
+                placeholder="Search Attendees"
+                class="form-control"
+                v-model="searchQuery"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-sm btn-outline-info"
+                  title="Pick a random attendee"
+                >
+                  <font-awesome-icon icon="random"></font-awesome-icon>
+                </button>
+                <button
+                  class="btn btn-sm btn-outline-info"
+                  title="Reset Search"
+                >
+                  <font-awesome-icon icon="undo"></font-awesome-icon>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="row justify-content-center">
       <div
         class="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
-        v-for="item in attendees"
+        v-for="item in filteredAttendees"
         :key="item.id"
       >
         <div class="card">
@@ -54,12 +87,21 @@ export default {
   data: function() {
     return {
       attendees: [],
+      searchQuery: "",
       userID: this.$route.params.userID,
       meetingID: this.$route.params.meetingID
     };
   },
   components: {
     FontAwesomeIcon
+  },
+  computed: {
+    filteredAttendees: function() {
+      const dataFilter = item =>
+        item.displayName.toLowerCase().match(this.searchQuery.toLowerCase()) &&
+        true;
+      return this.attendees.filter(dataFilter);
+    }
   },
   methods: {
     toggleStar: function(attendeeID) {
